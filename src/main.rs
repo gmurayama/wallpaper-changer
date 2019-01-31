6af8bec69b77dc3309f080ad6a81d4a918f000a0
Gustavo Murayama <gmurayama@outlook.com>
2019-01-31 21:21:22 -0200

    small structure to get args from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+extern crate wallpaper;
+extern crate chrono;
+extern crate structopt;
+
+use structopt::StructOpt;
+use chrono::prelude::*;
+
+fn parse_time(time: &str) -> NaiveTime {
+    NaiveTime::parse_from_str(time, "%H:%M").unwrap()
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "wallpaper", about = "An example of StructOpt usage.")]
+enum Opts {
+    #[structopt(name = "add")]
+    Add {
+        #[structopt(name = "path")]
+        path: String,
+        #[structopt(name = "time", parse(from_str = "parse_time"))]
+        time: NaiveTime
+    }
+}
+
+fn main() { 
+    let opt = Opts::from_args();
+    println!("{:?}", opt);
 }
